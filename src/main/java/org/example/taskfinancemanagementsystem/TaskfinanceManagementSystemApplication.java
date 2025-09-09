743a6a904aefb01b93679e13fc9ebcc87ee9ccf0
Davi Silva dos Santos <gssj9264@gmail.com>
2025-09-08 21:07:22 -0300

    InitialCommit

---
+++ src/main/java/org/example/taskfinancemanagementsystem/TaskfinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.taskfinancemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskfinanceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskfinanceManagementSystemApplication.class, args);
+    }
+
+}
